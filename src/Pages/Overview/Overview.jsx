c9462609f658d59f0a284cac30eb597efa300387
Dayveed021 <boskey021@gmail.com>
2023-10-17 23:26:53 -0500

    addToCart

--- src/Pages/Overview/Overview.jsx
+++ src/Pages/Overview/Overview.jsx
@@ -292,7 +292,7 @@ const Overview = () => {
                           icon={faCartShopping}
                           className="cart"
                           size="2x"
-                          onClick={() => handleAddToCart(courses)}
+                          onClick={() => handleAddToCart(courseArray.course)}
                         />
                       </button>
                     </div>
